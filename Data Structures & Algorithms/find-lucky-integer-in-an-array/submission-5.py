06da3e968741f25a8e570eec3f083a94987eef71
Aditya - Niranjan <adityaniranjan57@gmail.com>
2026-08-15 13:41:14 +0530

    Add: find-lucky-integer-in-an-array - submission-5

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        freq = {}
+
+        for num in arr:
+            freq[num] = freq.get(num, 0) + 1
+
+
+        lacky = -1
+
+        for num in freq:
+            if num == freq[num]:
+                lacky = max(lacky,num)
+
+        return lacky
+
+            # freq = [0] * 501
+
+            # for num in arr:
+            #     freq[num] += 1
+
+            # for num in range(500, 0, -1):
+            #     if freq[num] == num:
+            #         return num
+
+            # return -1
+
